3e361dfe008ec7b61b186dc068b33f563fc26cf6
MasRizkii <mohammadrizki.ramadhani24@gmail.com>
2026-09-17 01:05:25 +0700

    fix: route index requests to api/index.php lambda instead of static download

--- api/index.php
+++ api/index.php
@@ -1,3 +1,8 @@
 <?php
 
+// Pastikan direktori cache blade ada di /tmp untuk serverless Vercel
+if (!is_dir('/tmp/views')) {
+    @mkdir('/tmp/views', 0755, true);
+}
+
 require __DIR__ . '/../public/index.php';
